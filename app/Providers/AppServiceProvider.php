342040fc51f92d1ddd9fb76e27db3ce8d716283b
René Weiser <hello@reneweiser.de>
2026-03-08 14:44:14 +0100

    fix: remove duplicate activity log listener registration
    Laravel auto-discovers handle* methods in the Listeners directory, so
    the manual Event::subscribe() call and subscribe() method caused each
    event to be handled twice.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,11 +3,9 @@
 namespace App\Providers;
 
 use App\Http\Middleware\ResolveOrganization;
-use App\Listeners\RecordActivityListener;
 use Carbon\CarbonImmutable;
 use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\Rules\Password;
@@ -33,8 +31,6 @@ class AppServiceProvider extends ServiceProvider
     {
         $this->configureDefaults();
         $this->configureLivewire();
-
-        Event::subscribe(RecordActivityListener::class);
     }
 
     /**
